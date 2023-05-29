0cbe578343b6f925dd3681e6330f80f86c33a891
Doan <td.doan1212@gmail.com>
2023-05-30 02:12:24 +0700

    README.MD

--- 5-2023/B3_MacroFuntion/main.c
+++ 5-2023/B3_MacroFuntion/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define MAX 10
+// #define MAX 10
 
 #define SUM(a, b) ((a) + (b))
 
@@ -13,28 +13,31 @@
 CREATE_FUNC(test, "This is a test!")
 CREATE_FUNC(test2, "This is a abc function!")
 
-void test3(int a, int b)
-{
-    int c;
-    c = a + b;
-    printf("c = %d\n", c);
-}
+// void test3(int a, int b)
+// {
+//     int c;
+//     c = a + b;
+//     printf("c = %d\n", c);
+// }
 
 int main(int argc, char const *argv[])
 {
-    printf("MAX = %d\n", MAX);
+    // printf("MAX = %d\n", MAX);
 
-    int a = MAX + 5;
+    // int a = MAX + 5;
 
-    printf("a = %d\n", a);
+    // printf("a = %d\n", a);
 
-    printf("Tong a va b = %d\n", SUM(2, 3));
+    // printf("Tong a va b = %d\n", SUM(2, 3));
 
-    printf("Tong a va b = %d\n", SUM(2, 4));
+    // printf("Tong a va b = %d\n", SUM(2, 4));
 
     test();
 
     test2();
 
-    test3(4, 5);
+    // test3(4, 5);
+    // test3(5, 6);
+    // test3(6, 7);
+    
 }
