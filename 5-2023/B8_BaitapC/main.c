c6ca7602191cc4b4245afcfba8abd7e04cd1095e
Doan <td.doan1212@gmail.com>
2023-05-31 01:32:44 +0700

    My homework

--- 5-2023/B8_BaitapC/main.c
+++ 5-2023/B8_BaitapC/main.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdint.h>
-
 //  uint8_t array[] = {1, 3, 5, 2, 3, 4, 5, 6, 2, 6, 9, 6, 7};
 
 // so 1: xuat hien 1 lan
@@ -8,61 +5,46 @@
 // so 5: xuat hien 2 lan
 
 // cho 1 chuoi ky tu array[] = {1, 2, 3,4, 5, 6, 7, 8, 9}
+
 // Dao chuoi ky tu
 
-void sortList(uint8_t arr[], int n)
+
+#include <stdio.h>
+#include <stdint.h>
+#include "daoNguoc.c"
+#include "sortList.c"
+
+
+
+
+
+int main(int argc, const char *argv[])
 {
-    int i, j;
-    for (i = 0; i < n - 1; i++)
-    {
-        for (j = 0; j < n - i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                uint8_t temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
+
+    uint8_t arr[] = {1, 3, 5, 2, 3, 4, 5, 6, 2, 6, 9, 6, 7};
+    uint8_t size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Mang sau khi sap xep: ");
+    sortList(arr, size);
+    printfMang(arr, size);
 
 
 
+  
 
-void kiemTra(uint8_t array[], int n)
-{
-    int dem = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (array[i] == 5)
-        {
-            dem++;
-        }
-    }
+  
 
-    printf("So 1 xuat hien %d lan\n", dem);
-}
-
-int main(int argc, const char* argv[])
-{
-    uint8_t array[] = {1, 3, 5, 2, 3, 4, 5, 6, 2, 6, 9, 6, 7};
-
-    int n = sizeof(array) / sizeof(array[0]);
-
-    sortList(array, n);
-
-    printf("Mang tang dan: ");
-
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-
-    kiemTra(array, n);
+    printf("\nMang sau khi dao: ");
+    daoNguocMang(arr, size);
+    printfMang(arr, size);
 
 
+    uint8_t number;
+    nhapNumber(&number);
+    kiemTra(arr, size, number);
+
+    
+    printfMang(arr, size);
 
     return 0;
 }
