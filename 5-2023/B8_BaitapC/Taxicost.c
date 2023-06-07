43e07051bd666dad4f954dec2af0cce0733f01ef
Doan <td.doan1212@gmail.com>
2023-06-07 21:54:46 +0700

    Gia cuoc taxi

--- 5-2023/B8_BaitapC/Taxicost.c
+++ 5-2023/B8_BaitapC/Taxicost.c
@@ -5,39 +5,46 @@
 
 #include <stdio.h>
 
-#define COST 14000
-#define COST2 10000
-#define COST3 8000
+const int COST = 14000;
+const int COST2 = 10000;
+const int COST3 = 8000;
 
-int main()
+int tinhGiaCuoc(int soKm)
 {
-    int i_soKm;
-    int i_soTienPhaiTra = 1;
-    int i_soTienPhaiTra1 = 1;
 
-    printf("Nhap vao so km da di: ");
-    scanf("%d", &i_soKm);
+    int giaCuoc = 0;
 
-    i_soTienPhaiTra1 = (30 - 1) * COST2;
-
-    if (i_soKm < 14)
+    if (soKm < 0)
     {
-        i_soTienPhaiTra = i_soKm * COST;
-
-        printf("Gia cuoc xe = %d nghin", i_soTienPhaiTra);
+        printf("So km khong hop le.\n");
+        return giaCuoc;
     }
-    else if (i_soKm <= 30)
+    if (soKm < 14)
     {
-        i_soTienPhaiTra1 = (i_soKm - 1) * COST2 + COST;
-
-        printf("Gia cuoc xe = %d nghin", i_soTienPhaiTra1);
+        giaCuoc = soKm * COST;
+    }
+    else if (soKm <= 30)
+    {
+        giaCuoc = (soKm - 1) * COST2 + COST;
     }
     else
     {
-        i_soTienPhaiTra = (i_soKm - 30) * COST3 + i_soTienPhaiTra1 + COST;
-
-        printf("Gia cuoc xe = %d nghin", i_soTienPhaiTra);
+        giaCuoc = (soKm - 30) * COST3 + (30 - 1) * COST2 + COST;
     }
 
+    return giaCuoc;
+}
+
+int main()
+{
+
+    int soKm;
+
+    printf("Nhap vao so km da di: ");
+
+    scanf("%d", &soKm);
+
+    printf("Gia cuoc xe = %d", tinhGiaCuoc(soKm));
+    
     return 0;
 }
