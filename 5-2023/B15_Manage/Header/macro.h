83283d1023e05aaa4e1b2481ed9b9c1824354264
Doan <td.doan1212@gmail.com>
2023-07-08 17:47:11 +0700

    Manager

---
+++ 5-2023/B15_Manage/Header/macro.h
@@ -0,0 +1,13 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+#include <iostream>
+
+using namespace std;
+
+#define CHOOSE_KEY(content) \
+    cout << content;        \
+    cin >> number;          \
+    cin.ignore();
+
+#endif //
